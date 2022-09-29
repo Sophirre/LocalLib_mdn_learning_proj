1f1cd43570457978f1d42e0e0be4e7f37c54efe2
Sophirre <serik2005yt@gmail.com>
2022-09-29 16:06:11 +0300

    Initial commit

---
+++ catalog/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+
+from .models import Author, Genre, Book, BookInstance, Language
+
+admin.site.register(Book)
+admin.site.register(BookInstance)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(Language)
+# Register your models here.
