c88bd808b0dd8c331b5829bffcc13f37cabb32ee
S <serik2005yt@gmail.com>
2022-10-01 22:49:31 +0300

    Added urls and views configurations

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,9 @@
-from django.urls import  path
+from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('', views.index, name='index'),
+    path('books/', ''),
+    path('authors/', ''),
+    path('', ''),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Book, Author, BookInstance, Genre
+
+
+def index(req):
+    """
+    Function to render home page
+    """
+    # Generation of count some main objects
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    # Available books (status = 'a')
+
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    num_authors = Author.objects.all().count()
+
+    # HTML-Template rendering with integrated data
+
+    return render(req, "index.html",
+                  context={"num_books": num_books, "num_instances": num_instances,
+                           "num_instances_available": num_instances_available, "num_authors": num_authors})
